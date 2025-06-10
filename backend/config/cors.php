0cbc3669a9339e1e9e38ab7b66d374bc49ea0a51
NguyenHongHai <nguyenhonghaimwg2005@gmail.com>
2025-06-11 05:33:15 +0700

    Tạm thời commit chỉnh sửa cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,8 +3,8 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://127.0.0.1:5174',
-        'http://localhost:5174',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
